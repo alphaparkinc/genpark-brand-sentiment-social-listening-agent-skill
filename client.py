686b6627f4eacd4c08659fb6f5d209b6cc9a3dbe
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-07-28 12:42:19 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,5 @@
+class SocialListeningClient:
+    def analyze_sentiment(self, mentions: list) -> dict:
+        return {
+            "sentiment_ratio": {'positive': 0.5, 'negative': 0.5}
+        }
